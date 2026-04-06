495e36748048848b1174b8a85c75197c762ab629
Bidisha Pal <bidishapal84@gmail.com>
2026-04-07 00:13:05 +0530

    Implement error handling for SpaCy model loading
    Added error handling for loading SpaCy model.

--- nlp_utils.py
+++ nlp_utils.py
@@ -1,4 +1,11 @@
 import spacy
+
+try:
+    nlp = spacy.load("en_core_web_sm")
+except:
+    import os
+    os.system("python -m spacy download en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
